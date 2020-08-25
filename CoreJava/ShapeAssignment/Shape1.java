f74696c878ec58b5bb55817c1fe4d64b896c6eef
srujan77 <52212756+srujan77@users.noreply.github.com>
2020-08-25 17:42:34 +0530

    Create Shape1.java

---
+++ CoreJava/ShapeAssignment/Shape1.java
@@ -0,0 +1,26 @@
+package Day2;
+
+public  class Shape1 {
+
+	private String name;
+
+	public Shape1() {
+		name="name";
+	}
+
+	public Shape1(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
