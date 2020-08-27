5ed4924a542f74846d61e3bfac996305bd44f457
srujan77 <52212756+srujan77@users.noreply.github.com>
2020-08-27 20:16:13 +0530

    Create Employee.java

---
+++ CoreJava/EmployeeAssignment/Employee.java
@@ -0,0 +1,54 @@
+package com.pojo;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+	private int empId;
+	private String empName;
+	private Address address;
+	
+	public Employee() {
+		super();
+		empId=123;
+		empName="abc";
+		address=null;
+	}
+
+	public Employee(int empId, String empName, Address address) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+		this.address = address;
+	}
+
+	public int getempId() {
+		return empId;
+	}
+
+	public void setempId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getempName() {
+		return empName;
+	}
+
+	public void setempName(String empName) {
+		this.empName = empName;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", address=" + address + "]";
+	}
+	
+	
+}
